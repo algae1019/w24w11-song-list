8b1367adcc8683c977e1980906ec92133f3b2b1f
algae1019 <carl489@naver.com>
2024-11-27 15:25:52 +0900

    🍇 노래 제목 배열 사용

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Container from './Container.js'
 
 const App = () =>
 {
+  const songs = [
+    '사랑에 연습이 있었다면',
+    '사건의 지평선',
+    '사랑은 늘 도망가',
+  ]
+
   return (
     <div>
       <Header/>
-      <Playlist/>
-      <Container title="넌 나의 전부" index='1'/>
-      <Container title="이별하러가는 길" index='2'/>
-      <Container title="너라는 세상" index='3'/>
+      <Playlist title="프로그래밍하면서 듣고 싶은 노래"
+      listSong={songs}/>
     </div>
   )
 }
@@ -21,11 +25,16 @@ const Header = () =>
   )
 }
 
-const Playlist = () =>
+const Playlist = (props) =>
 {
   return (
     <div className='playlist'>
-      프로그래밍하면서 듣고싶은 노래
+    <div className='playlist'>{props.title}</div>
+    {
+      props.listSong.map((song, index) => (
+        <Container key={index} title={song} index={index}/>
+      ))
+    }
     </div>
   )
 }
